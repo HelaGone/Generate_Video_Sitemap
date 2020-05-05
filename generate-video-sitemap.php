76d34beb510a0d510caccf03f2524bbe6a3141aa
HelaGone <luah613@gmail.com>
2020-05-05 10:54:34 -0500

    scheduling cron

--- generate-video-sitemap.php
+++ generate-video-sitemap.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 	/**
-	 * Plugin Name: Generate XML File
+	 * Plugin Name: Generate Video Sitemap
 	 * Plugin URI:  https://github.com/HelaGone/Generate_Video_Sitemap
-	 * Description: Generate xml files for google video sitemap
+	 * Description: Generates a video sitemap for Google
 	 * Version:     1.0.0
 	 * Author:      Holkan Luna
-	 * Author URI:  https://cubeinthebox.com/
+	 * Author URI:  https://hela.dev/
 	 * License:     GPL2
 	 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
-	 * Text Domain: xml-file
+	 * Text Domain: video-sitemap
 	 * Domain Path: /languages
 	 */
 
 	function gxf_activation_fn(){
 
-	}	
+	}
 	register_activation_hook( __FILE__, 'gxf_activation_fn' );
 
 
@@ -22,31 +22,59 @@
 		if(is_file('../video_sitemap.xml')){
 			unlink('../video_sitemap.xml');
 		}
+
+		$timestamp = wp_next_scheduled( 'gvsm_cron_custom_hook' );
+		wp_unschedule_event( $timestamp, 'gvsm_cron_custom_hook' );
 	}
 	register_deactivation_hook( __FILE__, 'gxf_deactivation_fn' );
 
-	function gxf_count_video_posts($post_id, $post){
+
+	function gvsm_count_video_posts($post_id, $post){
 		if('video'===get_post_type($post_id)){
 			$args = array(
 				'post_type'=>'video',
-				'posts_per_page'=>1000,
+				'posts_per_page'=>500,
 				'post_status'=>'publish',
 				'orderby'=>'date',
 				'order'=>'DESC',
 				'date_query'=>array(
 					array(
-						'after'=>'2 days ago',
+						'after'=>'30 days ago',
 						'inclusive'=>true
 					)
 				)
 			);
 			$videos = get_posts($args);
-			gxf_generate_xml_file($videos);
+
+			// echo '<pre>';
+			// 	print_r($videos);
+			// echo '<pre>';
+			// die();
+
+			gvsm_generate_xml_file($videos);
 		}
 	}
-	add_action('publish_video', 'gxf_count_video_posts', 10, 2);
+	//TODO Implement CRON
+	add_action('gvsm_cron_custom_hook', 'gvsm_count_video_posts');
+	// add_action('publish_video', 'gvsm_count_video_posts', 10, 2);
 
-	function gxf_generate_xml_file($posts_object){
+	//Scheduling task after check for already scheduled tasks
+	if(!wp_next_scheduled('gvsm_cron_custom_hook')){
+		//The task is not assigned, then assign it
+		wp_schedule_event( time(), 'hourly', 'gvsm_cron_custom_hook' );
+	}
+
+	//Add interval to adjust how often this will run
+	function gvsm_add_cron_interval( $schedules ) {
+	    $schedules['five_seconds'] = array(
+				'interval' => 5,
+				'display'  => 'Cada Cinco Segundos'
+			);
+	    return $schedules;
+	}
+	add_filter( 'cron_schedules', 'gvsm_add_cron_interval' );
+
+	function gvsm_generate_xml_file($posts_object){
 		$xml = new DOMDocument('1.0', 'UTF-8');
 		$urlset = $xml->createElement('urlset');
 		$urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
@@ -68,7 +96,7 @@
 			$anv_width = '';
 
 			if(gettype($anvato_id)==='string'&&$anvato_id!==''){
-				$anv_data = gxf_get_video_data($anvato_id);
+				$anv_data = gvsm_get_video_data($anvato_id);
 
 				foreach($anv_data['docs'] as $key => $value){
 					$anv_title = $value['c_title_s'];
@@ -81,7 +109,7 @@
 					$anv_pub_date = gmdate('Y-m-d\TH:i:s\-06:00', $value['c_ts_publish_l']);
 
 					$anv_object = array('url'=>$anv_media_url,'poster'=>$anv_thumbnail,'title'=>$anv_title,'description'=>$anv_description);
-					$anv_player_url = gxf_get_anvato_player(json_encode($anv_object));
+					$anv_player_url = gvsm_get_anvato_player(json_encode($anv_object));
 
 
 					$url = $xml->createElement('url');
@@ -119,7 +147,7 @@
 				$yt_vid_url = 'https://www.youtube.com/watch?v='.$youtube_id;
 				$millis = time($vid_publish_date);
 				$pubDate = gmdate('Y-m-d\TH:i:s\-06:00', $millis);
-				$duration = gxf_get_youtube_duration($youtube_id);
+				$duration = gvsm_get_youtube_duration($youtube_id);
 
 				$url = $xml->createElement('url');
 				$url->appendChild($xml->createElement('loc', $vid_permalink));
@@ -156,7 +184,7 @@
 		$xml->save('../video_sitemap.xml');
 	}
 
-	function gxf_get_video_data($videoId){
+	function gvsm_get_video_data($videoId){
 		$requestUrl = 'https://api.anvato.net/v2/feed/K2KWCWUDQ5YXI2BNEFDWAAA?filters[]=obj_id:'.$videoId;
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, $requestUrl);
@@ -169,13 +197,13 @@
 		return $result;
 	}
 
-	function gxf_get_anvato_player($json){
+	function gvsm_get_anvato_player($json){
 		$decoded = json_decode($json);
 		$iframe_src = 'https://w3.cdn.anvato.net/player/prod/v3/anvload.html?key=' . base64_encode( json_encode( $decoded ) );
 		return $iframe_src;
 	}
 
-	function gxf_get_youtube_duration($videoId){
+	function gvsm_get_youtube_duration($videoId){
 		$apikey = 'AIzaSyC6zVkEIkrXXsvuAy5Z0QiDQld-ZPz1zVI';
 		$dur = file_get_contents('https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id='.$videoId.'&key='.$apikey);
 		$vid_duration = json_decode($dur, true);
@@ -183,12 +211,14 @@
 		foreach ($vid_duration['items'] as $vidTime) {
 			$duration = $vidTime['contentDetails']['duration'];
 		}
-		return gfx_ISO8601ToSeconds($duration);
+		return gvsm_ISO8601ToSeconds($duration);
 	}
 
-	function gfx_ISO8601ToSeconds($ISO8601){
+	function gvsm_ISO8601ToSeconds($ISO8601){
     	$interval = new \DateInterval($ISO8601);
     	return ($interval->d * 24 * 60 * 60) + ($interval->h * 60 * 60) + ($interval->i * 60) + $interval->s;
 	}
+
+
 
 ?>
